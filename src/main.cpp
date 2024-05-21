093ab90f9cb1183485ff357baae87cc3587dc69a
Ionut Gaina <gainaionutz@gmail.com>
2024-05-22 01:23:05 +0300

    reformat

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "utils.hpp"
 #include <Arduino.h>
 #include <Keypad.h>
 #include <LiquidCrystal_I2C.h>
@@ -5,53 +6,122 @@
 #include <SPI.h>
 #include <TMRpcm.h>
 #include <Wire.h>
-#include <utils.hpp>
 
-// LCD
+// LCD setup
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-const int ROW_NUM = 4;    // four rows
-const int COLUMN_NUM = 4; // four columns
-
-char keys[ROW_NUM][COLUMN_NUM] = {{'1', '2', '3', 'A'},
-                                  {'4', '5', '6', 'B'},
-                                  {'7', '8', '9', 'C'},
-                                  {'*', '0', '#', 'D'}};
-
-byte pin_rows[ROW_NUM] = {9, 8, 7,
-                          6}; // connect to the row pinouts of the keypad
-byte pin_column[COLUMN_NUM] = {
-    5, 4, 3}; // connect to the column pinouts of the keypad
-
+// Keypad setup
+const int ROW_NUM = 4;
+const int COLUMN_NUM = 3;
+char keys[ROW_NUM][COLUMN_NUM] = {
+    {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'}, {'*', '0', '#'}};
+byte pin_rows[ROW_NUM] = {9, 8, 7, 6};
+byte pin_column[COLUMN_NUM] = {5, 4, 3};
 Keypad keypad =
     Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
 
-// global variables
+// Global variables
 bool error = false;
-
-char mykey = ' ';
 String entered_pin = "";
 String current_pin = "";
 File passwordFile;
+STAGE stage = DEACTIVATED;
 
-STAGE stage = STAGE::DEACTIVATED;
+// Function prototypes
+String read_pin();
+bool set_new_pin(const String &new_pin);
+void beep();
+void correctPIN();
+void incorrectPIN();
+bool check_pin();
+bool read_keypad();
+bool read_keypad_reset();
+void deactivated_stage();
+void activated_stage();
+void settings_stage();
+
+void setup() {
+  // Initialize LCD
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Initializing...");
+
+  // Initialize Serial
+  Serial.begin(9600);
+
+  // Initialize PIR sensor, LED, and Audio pin
+  pinMode(PIR_PIN, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, HIGH);
+  pinMode(AUDIO_PIN, OUTPUT);
+  digitalWrite(AUDIO_PIN, HIGH);
+
+  // Initialize MicroSD
+  pinMode(MICRO_SD_PIN, OUTPUT);
+  if (!SD.begin(MICRO_SD_PIN)) {
+    error = true;
+    Serial.println("SD Card Initialization Failed");
+  }
+
+  // Read the current PIN
+  current_pin = read_pin();
+  if (current_pin.length() != PIN_LENGTH && !set_new_pin(DEFAULT_PIN)) {
+    error = true;
+  } else if (current_pin.length() != PIN_LENGTH) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Resetting PIN");
+    delay(2000);
+  }
+
+  // Display error if any
+  if (error) {
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Error");
+    lcd.setCursor(0, 1);
+    lcd.print("Check Serial");
+    while (1) {
+      delay(100);
+    }
+  }
+  lcd.clear();
+}
+
+void loop() {
+  switch (stage) {
+  case SETTINGS:
+    settings_stage();
+    break;
+  case DEACTIVATED:
+    deactivated_stage();
+    break;
+  case ACTIVATED:
+    activated_stage();
+    break;
+  default:
+    break;
+  }
+  delay(100);
+}
 
 String read_pin() {
   passwordFile = SD.open("pass.txt", FILE_READ);
   if (passwordFile) {
-    current_pin = passwordFile.readString();
+    String pin = passwordFile.readString();
     passwordFile.close();
-    return current_pin;
+    return pin;
   } else {
     Serial.println("Error opening pass.txt for reading");
     return DEFAULT_PIN;
   }
 }
 
-bool set_new_pin(String new_pin) {
+bool set_new_pin(const String &new_pin) {
   if (new_pin.length() == PIN_LENGTH) {
     current_pin = new_pin;
-    passwordFile = SD.open("pass.txt", O_WRITE | O_CREAT | O_TRUNC);
+    passwordFile = SD.open("pass.txt", FILE_WRITE);
     if (passwordFile) {
       passwordFile.print(current_pin);
       passwordFile.close();
@@ -72,83 +142,21 @@ void beep() {
   noTone(AUDIO_PIN);
 }
 
-void setup() {
-  /* LCD INIT */
-  lcd.init();
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Initializing...");
-
-  /* KEYPAD INIT */
-
-  Serial.begin(9600);
-
-  /* PIR SENSOR INIT */
-  pinMode(PIR_PIN, INPUT);
-
-  /* LED */
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, HIGH);
-
-  /* AUDIO */
-  pinMode(AUDIO_PIN, OUTPUT);
-  digitalWrite(AUDIO_PIN, HIGH);
-
-  /* MicroSD */
-  pinMode(MICRO_SD_PIN, OUTPUT);
-  if (!SD.begin(MICRO_SD_PIN)) {
-    error = true;
-    Serial.println("SD Card Initialization Failed");
-  }
-
-  /* Read the current PIN */
-  current_pin = read_pin();
-
-  if (current_pin.length() != PIN_LENGTH) {
-    if (!set_new_pin(DEFAULT_PIN)) {
-      error = true;
-    } else {
-      lcd.clear();
-      lcd.setCursor(0, 0);
-      lcd.print("Resetting PIN");
-      delay(2000);
-    }
-  }
-
-  /* END SETUP */
-  if (error) {
-    lcd.clear();
-    lcd.setCursor(0, 0);
-    lcd.print("Error");
-    lcd.setCursor(0, 1);
-    lcd.print("Check Serial");
-    while (1)
-      ;
-  }
-  lcd.clear();
-}
-
-void correctPIN() // do this if correct PIN entered
-{
+void correctPIN() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("* Correct PIN *");
-
   digitalWrite(LED_PIN, HIGH);
-
   delay(1000);
   lcd.clear();
   lcd.print("Enter PIN...");
 }
 
-void incorrectPIN() // do this if incorrect PIN entered
-{
+void incorrectPIN() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print(" * Try again *");
-
   digitalWrite(LED_PIN, LOW);
-
   delay(1000);
   lcd.clear();
   lcd.print("Enter PIN...");
@@ -169,45 +177,37 @@ bool read_keypad() {
   if (key || entered_pin.length() == PIN_LENGTH) {
     beep();
     if (key == '#' || entered_pin.length() == PIN_LENGTH) {
-      if (entered_pin.length() == 0) {
-        if (stage == STAGE::DEACTIVATED) {
-          stage = STAGE::SETTINGS;
-          return false;
-        }
+      if (entered_pin.length() == 0 && stage == DEACTIVATED) {
+        stage = SETTINGS;
+        return false;
       }
-
       if (check_pin()) {
         entered_pin = "";
         return true;
       }
       entered_pin = "";
-
     } else if (key == '*') {
       entered_pin = "";
     } else {
       entered_pin += key;
     }
   }
-
   lcd.setCursor(0, 1);
   for (unsigned int i = 0; i < entered_pin.length(); i++) {
     lcd.print("*");
   }
-
   return false;
 }
 
 bool read_keypad_reset() {
-  // change the PIN
   char key = keypad.getKey();
   if (key) {
     beep();
     if (key == '#') {
       if (entered_pin.length() == 0) {
-        stage = STAGE::DEACTIVATED;
+        stage = DEACTIVATED;
         return false;
       }
-
       if (!set_new_pin(entered_pin)) {
         lcd.clear();
         lcd.setCursor(0, 0);
@@ -237,27 +237,22 @@ bool read_keypad_reset() {
 }
 
 void deactivated_stage() {
-  // The alarm is not activated
   digitalWrite(LED_PIN, HIGH);
-
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("PIN to activate:");
-
   if (read_keypad()) {
-    stage = STAGE::ACTIVATED;
+    stage = ACTIVATED;
   }
 }
 
 void activated_stage() {
-  // The alarm is activated
   digitalWrite(LED_PIN, LOW);
-
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("PIN to deactivate:");
   if (read_keypad()) {
-    stage = STAGE::DEACTIVATED;
+    stage = DEACTIVATED;
   }
 }
 
@@ -265,24 +260,7 @@ void settings_stage() {
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("Enter new PIN");
-
   if (read_keypad_reset()) {
-    stage = STAGE::DEACTIVATED;
+    stage = DEACTIVATED;
   }
-}
-void loop() {
-  switch (stage) {
-  case STAGE::SETTINGS:
-    settings_stage();
-    break;
-  case STAGE::DEACTIVATED:
-    deactivated_stage();
-    break;
-  case STAGE::ACTIVATED:
-    activated_stage();
-    break;
-  default:
-    break;
-  }
-  delay(100);
 }
--- src/utils.hpp
+++ src/utils.hpp
@@ -1,27 +1,20 @@
 #ifndef AF01E42E_D04B_428E_9046_3AC3100CE31A
 #define AF01E42E_D04B_428E_9046_3AC3100CE31A
-// PIR Sensor
+
+// Constants and Pin Definitions
 const int PIR_PIN = 2;
-
-// AUDIO
 const int AUDIO_PIN = A1;
-
-// LED
 const int LED_PIN = A0;
-
-// MicroSD
 const int MICRO_SD_PIN = 10;
-
-// PIN
 const String DEFAULT_PIN = "1234";
 const int PIN_LENGTH = 4;
 
-enum STAGE
-{
-    DEACTIVATED,
-    ACTIVATED,
-    ALARM,
-    SETTINGS
+// Enum for stages
+enum STAGE {
+  DEACTIVATED,
+  ACTIVATED,
+  ALARM,
+  SETTINGS
 };
 
 #endif /* AF01E42E_D04B_428E_9046_3AC3100CE31A */
